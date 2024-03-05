0f37079fc8266e34bff2e1350932bb714f2074b6
qi_gu <252915823@qq.com>
2024-03-05 14:08:57 +0800

    Update auto_sign.py

--- auto_sign.py
+++ auto_sign.py
@@ -1,22 +1,21 @@
 from requests import post
-from time import sleep
+import re
 
-sleep(3)
-file = open('C:/autosign_log.txt', 'w')
 header = {'content-type': 'application/json;charset=UTF-8'}
-port = ''	# 填入clash中的端口号
-cookie = {'koa:sess': '', 'koa:sess.sig': ''}	# 填入GLaDOS网页对应的cookie
-proxy = {'http': '127.0.0.1:' + port, 'https': '127.0.0.1:' + port}
+cookie = {'koa:sess': '', 'koa:sess.sig': ''}
+proxy = {'http': '127.0.0.1:7890', 'https': '127.0.0.1:7890'}
 try:
 	x = post('https://glados.rocks/api/user/checkin', cookies=cookie, headers=header, data='{"token":"glados.one"}')
 except:
 	try:
 		x = post('https://glados.rocks/api/user/checkin', cookies=cookie, headers=header, data='{"token":"glados.one"}', proxies=proxy)	# 防止开了系统代理
 	except:
-		file.write('fail')
 		input('断网了！')
 		exit(1)
-file.write(x.text)
-if not (("Checkin! Get 1 Day" in x.text) or ('Please Try Tomorrow' in x.text)):
+
+if not (("Checkin!" in x.text) or ('Please Try Tomorrow' in x.text)):
 	print(x.text)
 	input('签到失败！')
+else:
+    print(re.search('Got [0-9]* Points', x.text).group())
+    input()
